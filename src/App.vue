62aace90b02d949af0d4c4b91469519f948118ec
Jehu Casimiro <96979598+J1hu@users.noreply.github.com>
2024-01-21 15:38:10 +0800

    Apply application layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-// Main Components
+import SideBar from './components/SideBar.vue';
+
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <!-- HEADER -->
+    <div class="p-3">Header</div>
+    
+    <!-- MAIN CONTENT -->
+    <div class="grid grid-cols-5 h-screen">
+        <!-- SIDEBAR -->
+        <div class="bg-orange-400 col-span-1 p-3">
+            <SideBar/>
+        </div>
+        <!-- CONTENT -->
+        <div class="bg-blue-400 col-span-4 p-3">
+            Content
+        </div>
+    </div>
 </template>
 
 <style scoped>
