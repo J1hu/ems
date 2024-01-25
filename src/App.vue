50cb64fa4255ab32ef76e5b86894361f84dc0744
Jehu Casimiro <96979598+J1hu@users.noreply.github.com>
2024-01-25 14:25:53 +0800

    Static header, installed heroicons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import SideBar from './components/SideBar.vue';
+import MainHeader from './components/MainHeader.vue';
 
 </script>
 
 <template>
-    <nav class="p-3 fixed bg-red-400 w-full">
-        Header
+    <nav class="fixed w-full">
+        <MainHeader />
     </nav>
     
     <div class="grid grid-cols-6 h-screen pt-12">
         <div class="col-span-1">
-            <SideBar/>
+            <SideBar />
         </div>
         <div class="bg-blue-400 col-span-5 p-3">
             Content
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { BellIcon } from '@heroicons/vue/24/solid'
+
+</script>
+
+<template>
+    <div class="flex justify-between px-3 py-3 border border-b-gray-300 bg-white">
+        <div>
+            <span class="text-red-500">Qstrike</span> Macrodashboard
+        </div>
+        <div class="flex flex-row">
+            <div class="w-5 pt-0.5 mr-3">
+                <BellIcon class="fill-gray-400" />
+            </div>
+            <div>
+                Admin Account
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
